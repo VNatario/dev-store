687f20810817c4e62258372dba0e966f02f7ea1c
Vitor Natario <natariovitor@gmail.com>
2023-11-18 21:06:43 -0300

    feat: create header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br" className={inter.variable}>
-      <body>{children}</body>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
