0a9314a05353136180b8d211009d0dbba8e70496
Vitor Natario <natariovitor@gmail.com>
2023-11-21 19:10:28 -0300

    feat: Add Fetch API Wrapper

---
+++ src/env.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+})
+
+//Verifica se as variáveis de ambiente estão corretas de acordo com o schema
+const parsedEnv = envSchema.safeParse(process.env)
+
+if (!parsedEnv.success) {
+  console.error(
+    'Invalid environment variables',
+    //Exibe os erros de uma forma mais amigável
+    parsedEnv.error.flatten().fieldErrors
+  )
+  //Encerra o processo
+  throw new Error('Invalid environment variables')
+}
+
+export const env = parsedEnv.data
