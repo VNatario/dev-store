1e5b342c8b520c7995ca2fee7264a922ab68ce65
Vitor Natario <natariovitor@gmail.com>
2023-12-01 00:53:35 -0300

    fix: change type productId in CartContext

--- src/context/cart-context.tsx
+++ src/context/cart-context.tsx
@@ -2,13 +2,13 @@
 import { ReactNode, createContext, useContext, useState } from 'react'
 
 interface CartItem {
-  productId: string
+  productId: number
   quantity: number
 }
 
 interface CartContextType {
   items: CartItem[]
-  addToCart: (ProductID: string) => void
+  addToCart: (ProductID: number) => void
 }
 
 const CartContext = createContext({} as CartContextType)
@@ -16,7 +16,7 @@ const CartContext = createContext({} as CartContextType)
 export function CartProvider({ children }: { children: ReactNode }) {
   const [cartItems, setCartItems] = useState<CartItem[]>([])
 
-  function addToCart(productId: string) {
+  function addToCart(productId: number) {
     setCartItems((prevState) => {
       const productInCart = prevState.some(
         (item) => item.productId === productId
