311ee3b9d874ff708c994afbe90646869a5147e8
Vitor Natario <natariovitor@gmail.com>
2023-12-02 11:47:34 -0300

    feat: create search route

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+import { NextRequest } from 'next/server'
+import data from '../data.json'
+
+export async function GET(request: NextRequest) {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  const { searchParams } = request.nextUrl
+
+  //query validation
+  const query = z.string().parse(searchParams.get('q'))
+
+  const products = data.products.filter((product) => {
+    return product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+  })
+
+  return Response.json(products)
+}
