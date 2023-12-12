394f5c8c53fd0507b39e2c6a713647d320cfb1db
Vitor Natario <natariovitor@gmail.com>
2023-12-12 14:26:18 -0300

    feat: validating environment variables with @t3-oss/env-nextjs

--- src/env.ts
+++ src/env.ts
@@ -1,21 +1,31 @@
+// src/env.mjs
+import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
-const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-  APP_URL: z.string().url(),
+export const env = createEnv({
+  /*
+   * Serverside Environment variables, not available on the client.
+   * Will throw if you access these variables on the client.
+   */
+  server: {
+    APP_URL: z.string().url(),
+  },
+  /*
+   * Environment variables available on the client (and server).
+   *
+   * 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+   */
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  },
+  /*
+   * Due to how Next.js bundles environment variables on Edge and Client,
+   * we need to manually destructure them to make sure all are included in bundle.
+   *
+   * 💡 You'll get type errors if not all variables from `server` & `client` are included here.
+   */
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 })
-
-//Verifica se as variáveis de ambiente estão corretas de acordo com o schema
-const parsedEnv = envSchema.safeParse(process.env)
-
-if (!parsedEnv.success) {
-  console.error(
-    'Invalid environment variables',
-    //Exibe os erros de uma forma mais amigável
-    parsedEnv.error.flatten().fieldErrors
-  )
-  //Encerra o processo
-  throw new Error('Invalid environment variables')
-}
-
-export const env = parsedEnv.data
